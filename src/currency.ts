e619f8b3f96eca206fc7eb0a435deeed4e854e32
allan-mobley-jr <allan.mobley.jr@mobsites.com>
2021-02-23 04:17:54 -0500

    Add currency

---
+++ src/currency.ts
@@ -0,0 +1,14 @@
+/**
+ * US currency formatter.
+ */
+export const usCurrencyFormatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD'
+});
+
+/**
+ * Converts a number to a US currency string.
+ */
+export function toUSCurrency(num: number) {
+  return usCurrencyFormatter.format(num);
+}
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+/**
+ * @license
+ * Copyright (c) 2021 QBCart Inc. All rights reserved.
+ * This code can only be used and/or distributed with express permission.
+ */
+
+import { usCurrencyFormatter, toUSCurrency } from './currency';
+
+export { usCurrencyFormatter, toUSCurrency };
